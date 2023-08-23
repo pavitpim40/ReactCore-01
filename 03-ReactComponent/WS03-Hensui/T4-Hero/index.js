c2d977b5b4d948b0513131edbc930e2cd6c475ba
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-23 15:33:32 +0700

    image

--- 03-ReactComponent/WS03-Hensui/T4-Hero/index.js
+++ 03-ReactComponent/WS03-Hensui/T4-Hero/index.js
@@ -1,7 +1,14 @@
 // #1 React : CreateElement
-
+function Hero() {
+    return (
+        <div className='img__container'>
+            <img src='./image.png' />
+        </div>
+    );
+}
 // #2 ReactDOM : createRoot
 const domNode = document.getElementById('root');
 const root = ReactDOM.createRoot(domNode);
 
 // #3 ReactDOM : TakeOver for Render Task
+root.render(<Hero />);

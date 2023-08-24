5a088befd0b95a1c22a602ed14f5b5297e231457
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-24 16:12:58 +0700

    lab05

--- 04-Props/L5-FriendList/main.js
+++ 04-Props/L5-FriendList/main.js
@@ -1,2 +1,18 @@
+const friends = ['Tony', 'Steve', 'Peter', 'Natasha'];
+
+function FriendList() {
+    return (
+        <div>
+            {friends.map((name) => (
+                <div className='card'>
+                    <div className='card2'>{name}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
 const domRoot = document.getElementById('root');
 const root = ReactDOM.createRoot(domRoot);
+
+root.render(<FriendList />);

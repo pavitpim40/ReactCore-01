04ecf2a844d0e804c4945ac91361eba47560db32
Pavit Pimchanagul <pavitpim@gmail.com>
2023-08-24 16:51:10 +0700

    finish lab

--- 04-Props/L6-Post/main.js
+++ 04-Props/L6-Post/main.js
@@ -1,2 +1,60 @@
+// ให้สร้างตัวแปร Array เก็บข้อมูล Post ที่เป็น Object มี key คือ id, message, วันที่ Post, ชื่อคนโพสต์
+// นำ ArrayPost มา Render ใน list แล้วแสดงบนหน้า UI
+
+const posts = [
+    { id: 1, message: 'หลังธูปคือรูปเธอ', date: '2023-08-23', author: 'tar' },
+    {
+        id: 2,
+        message: 'ความสำเร็จอยู่ไม่ไกล แต่อยู่ไหนก็ไม่รู้',
+        date: '2023-07-23',
+        author: 'alac',
+    },
+    { id: 3, message: 'เข้าทุกคาบ แต่ไม่ทราบอะไรเลย', date: '2023-07-23', author: 'pete' },
+];
+
+function PostItem(props) {
+    // props = { data : postObj }
+    // postObj = {id:number, message:string,date:string, author:string}
+    return (
+        <div>
+            <h1>{props.data.message}</h1>
+            <p>
+                {props.data.author.toUpperCase()} :{props.data.date}
+            </p>
+        </div>
+    );
+}
+// #2
+function PostList() {
+    return (
+        <div className='container'>
+            {posts.map((postObj, index) => (
+                <PostItem
+                    key={postObj.id}
+                    // key={index}
+                    data={postObj}
+                />
+            ))}
+        </div>
+    );
+}
+
+// #1
+// function PostList() {
+//     return (
+//         <div className='container'>
+//             {posts.map((postObj) => (
+//                 <div>
+//                     <h1>{postObj.message}</h1>
+//                     <p>
+//                         {postObj.author.toUpperCase()} :{postObj.date}
+//                     </p>
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// }
 const domRoot = document.getElementById('root');
 const root = ReactDOM.createRoot(domRoot);
+
+root.render(<PostList />);
--- 04-Props/L7-SocialFeed/main.js
+++ 04-Props/L7-SocialFeed/main.js
@@ -1,2 +1,64 @@
+const posts = [
+    { id: 1, text: 'Happy NewYear', author: 'Tar', date: '2023-01-23' },
+    { id: 2, text: 'Happy BirthDay', author: 'Alac', date: '2023-02-23' },
+    { id: 3, text: 'Happy Valentine', author: 'Mix', date: '2023-03-23' },
+];
+
+// #1 รับ Props
+// function Card(props) {
+//     return (
+//         <div className='card'>
+//             <div>{props.text}</div>
+//             <div className='card__footer'>
+//                 <p>Author :{props.author} </p>
+//                 <p>{props.date}</p>
+//             </div>
+//         </div>
+//     );
+// }
+
+// #2 รับ Props แล้วมา Destructure
+// function Card(props) {
+//     const { text, author, date } = props;
+
+//     // const text = props.text
+//     // const author = props.author
+//     // const date = props.date
+//     return (
+//         <div className='card'>
+//             <div>{text}</div>
+//             <div className='card__footer'>
+//                 <p>Author :{author} </p>
+//                 <p>{date}</p>
+//             </div>
+//         </div>
+//     );
+// }
+
+// #3
+function Card({ text, author, date }) {
+    return (
+        <div className='card'>
+            <div>{text}</div>
+            <div className='card__footer'>
+                <p>Author :{author} </p>
+                <p>{date}</p>
+            </div>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div className='container'>
+            {posts.map((obj) => (
+                <Card key={obj.id} text={obj.text} author={obj.author} date={obj.date} />
+            ))}
+        </div>
+    );
+}
+
 const domRoot = document.getElementById('root');
 const root = ReactDOM.createRoot(domRoot);
+
+root.render(<App />);
